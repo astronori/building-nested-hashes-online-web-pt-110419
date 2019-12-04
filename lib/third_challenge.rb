aae1ba43858c841f4d8c0df61fd56bca850842db
kenneth gil <astronori@gmail.com>
2019-12-04 01:39:47 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,8 +11,8 @@ def third_challenge
         name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => [
-        {name: "Benvolio", age: "17", attribute: "worried"},
-        {name: "Mercutio", age: "18", attribute: "hot-headed"}
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
         ]
    }, 
    :capulet => {
@@ -26,7 +26,8 @@ def third_challenge
         name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => [
-        {name: "Steven", age: "30", }
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
         ] 
    }
   }

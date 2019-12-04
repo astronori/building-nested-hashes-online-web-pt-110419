feabb4e0956ad91a6d6488e0425afce55beebaf7
kenneth gil <astronori@gmail.com>
2019-12-04 01:33:06 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,8 @@ def third_challenge
       :hero => {
         name: "Romeo", age: "15", status: "alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {name: "Benvolio", age: }]
    }, 
    :capulet => {
       :patriarch => {
